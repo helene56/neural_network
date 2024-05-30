6f8ae89c0af7bcdce0357f61fd82efb149208a95
helene <helene.maja@hotmail.dk>
2024-05-30 20:45:05 +0200

    framework setup

---
+++ neural_network.py
@@ -0,0 +1,11 @@
+class neuralNetwork:
+    def __init__():
+        pass
+
+    # train the neural network
+    def train():
+        pass
+    
+    # query the neural network
+    def query():
+        pass
